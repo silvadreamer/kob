36260e989582b4f6bf2a5efa0dd0e38ea9348c71
yh Futaba <1957915633@qq.com>
2023-04-08 22:13:24 +0800

    创建项目

---
+++ backend/src/main/java/com/example/backend/controller/pk/IndexController.java
@@ -0,0 +1,13 @@
+package com.example.backend.controller.pk;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author Silva31
+ * @date 2023/4/8 22:09
+ */
+
+@Controller
+public class IndexController
+{
+}
